58eec3586f95386317804da7848d2a4c3ea06d35
Randall Wolfe <randall.a.wolfe@gmail.com>
2026-08-27 16:38:02 -0700

    added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  modules: ["@nuxt/eslint"],
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  compatibilityDate: "2025-07-15",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
